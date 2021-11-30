e9ee2026261658ee3d3e0ae084977879fe2cb252
Jacob Mann <gothub@h-mail.us>
2021-11-29 22:58:30 -0800

    refactored test_cli.py, so initiating the parser instance was done with a decorator

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,8 +1,13 @@
 from argparse import Action, ArgumentParser
 
+known_drivers = ['local', 's3']
+
+
 class DriverAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("Unkown driver. Available drivers are 'local' and 's3'")
         namespace.driver = driver.lower()
         namespace.destination = destination
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,36 +1,51 @@
 
 #$ pgbackup postgres://bob@example.com5432/db_one --driver s3 backups
 import pytest
+
 from pgbackup import cli
 
 url = "postgres://bob@example.com5432/db_one"
 
+@pytest.fixture
+def parser():
+    return cli.create_parser()
 
-def test_parser_wihtout_driver():
+def test_parser_wihtout_driver(parser):
     """
     Without a specified driver the parser will exiti
     """
     with pytest.raises(SystemExit):
-        parser = cli.create_parser()
         parser.parse_args([url])
 
 
-def test_parser_with_driver():
+def test_parser_with_driver(parser):
     """
-    The parser wo; exit if it recieves a driver without a destination
+    The parser will exit if it receives a driver without a destination
     """
-
-    parser = cli.create_parser()
     with pytest.raises(SystemExit):
         parser.parse_args([url, "--driver", "local"])
 
 
-def test_parser_with_driver_and_destination():
+def test_parser_wihtout_driverst_partner_with_unkown_driver(parser):
+    """
+    The parser will exit if the driver name is unknownn
+    """ 
+    with pytest.raises(SystemExit):
+        parser.parse_args([url, "--driver", "azure", "destination"])
+
+
+def test_parser_with_known_drivers(parser):
+    """
+    The Parser will not exit if the driver name is known
+    """
+    for driver in ["local","s3"]:
+        assert parser.parse_args([url, '--driver', driver, 'destination'])
+
+
+def test_parser_with_driver_and_destination(parser):
     """
     The parser will not exit if it recieves a driver and destination
     """
-
-    parser = cli.create_parser()
     args = parser.parse_args([url, "--driver", "local", "/some/path"])
 
     assert args.url == url
